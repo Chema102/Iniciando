938834790db4427cec1c0084e1f8eece494a1a7b
Jose Maria Quiñonez Guevara <jose_102102@hotmail.com>
2020-11-15 23:48:45 -0800

    la diferencia entre let y var

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,15 @@
+/* es mejor sentenciar variables con "let", porque si lo hacemos con "var" se 
+convierte en una varible global, pero con "let" es una variable de bloque  */
 
-var numero1 = 5;
-var numero2 = 5;
+var numero1="hola";
+var numero2="hola2";
 
-console.log(numero2);
+let numero3 = "hola mundo" ;
+let numero4 = 132;
+
+console.log(window.numero1);
+console.log(window.numero2);
+
+console.log(window.numero3);
+console.log(window.numero4);
+  
