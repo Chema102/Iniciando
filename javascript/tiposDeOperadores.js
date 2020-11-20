c167399606706f074e766d5c159fd0ca10ad0d4b
Jose Maria Quiñonez Guevara <jose_102102@hotmail.com>
2020-11-20 15:09:33 -0800

    tipos de operadores

---
+++ javascript/tiposDeOperadores.js
@@ -0,0 +1,36 @@
+// operadires logicos + - * / %
+console.log(1+1);
+console.log(1-1);
+console.log(1*1);
+console.log(1/1);
+//esto lo que saca es el residuo de la divicion
+console.log(15 % 4);
+
+//relacionales >, <, >=, <=, =, ==, ===, !=, !==
+
+console.log(8 > 5);
+console.log(8 <5);
+console.log(8 >= 5);
+console.log(8 >= 9);
+
+/*
+=  1 igual es asignacion de variable
+== 2 iguales es comparacion de valores
+=== 3 iguales es comparacion de valores y tipo de dato
+*/
+let i = 0;
+i = i+1;
+i++;
+console.log(i);
+i--;
+console.log(i);
+
+/*
+! - NOT  lo niega, lo que es verdadero lo vuelve a falso y viseversa
+|| - Or si una de las condiciones se cumple es verdadero
+&& - AND se tienen que cumplir todas las condiciones para que sea verdadero
+ */
+
+ console.log(!true);
+ console.log(!false);
+ 
